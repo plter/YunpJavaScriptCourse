4f8521dc877485ad0950ebd285f3682452114f0c
plter <xtiqin@163.com>
2017-09-19 15:30:21 +0800

    JavaScript第一课

---
+++ 20170909/HelloWorld/app.js
@@ -0,0 +1,19 @@
+// console.log("Hello World");
+
+// window.onload = function () {
+//     $("#container").html("Hello World");
+//
+// };
+
+
+// $(document).ready(function () {
+//     $("#container").html("Hello World");
+// });
+
+
+// function bodyLoaded() {
+//     $("#container").html("Hello World");
+// }
+
+
+$("#container").html("Hello World");
---
+++ 20170909/Var/app.js
@@ -0,0 +1,18 @@
+var a = 10;
+a = 3.14;
+a = "Hello World";
+// var b = 20;
+// a = 100;
+// var c = a + b;
+//
+// console.log(a, b, c);
+
+//ES5 ES6
+
+var MATH_PI = 3.14;
+// MATH_PI = 10;
+
+// const MATH_PI = 3.14;
+// MATH_PI = 10;
+
+console.log(MATH_PI);
