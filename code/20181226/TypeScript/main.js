d478439df0a849c1e1addec76e05047dc4e0fa8b
plter <xtiqin@163.com>
2018-12-27 08:17:11 +0800

    添加typescript课程源码

---
+++ code/20181226/TypeScript/main.js
@@ -0,0 +1,10 @@
+var Hello = /** @class */ (function () {
+    function Hello() {
+    }
+    Hello.prototype.sayHello = function () {
+        console.log("Hello World 2");
+    };
+    return Hello;
+}());
+new Hello().sayHello();
+//# sourceMappingURL=main.js.map
